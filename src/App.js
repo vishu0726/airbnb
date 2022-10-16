a8d03495c18656f813a4cea4a2cabc3f346cf0f3
vishal <vishalchaware94@gmail.com>
2022-10-16 08:41:17 +0530

    added media query

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter,Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    
     <div className="app">
+      <BrowserRouter>
       <Header/>
-      <Home/> 
-      <Footer/>
-     {/* header */}
-     {/* banner */}
-     {/* cards */}
-     {/* footer */}
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='*' element={<h2>Page is not found</h2>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
